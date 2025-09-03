09e28ae5e4354ebe59cadbb3308652f4c0998c85
Gabriel Maglia <93683370+gabiMaglia@users.noreply.github.com>
2025-09-03 17:35:27 -0300

    fix: provide request config for next-intl

--- i18n.js
+++ i18n.js
@@ -1,7 +1,12 @@
+import {getRequestConfig} from 'next-intl/server';
+
 export const locales = ['en', 'es'];
 export const defaultLocale = 'en';
 
-export default {
-  locales,
-  defaultLocale
-};
+export default getRequestConfig(async ({locale}) => {
+  const resolvedLocale = locale ?? defaultLocale;
+  return {
+    locale: resolvedLocale,
+    messages: (await import(`./messages/${resolvedLocale}.json`)).default
+  };
+});
